169e6dec1d4ed62e95daee0f8499047abca1ef77
user <ianbjs@gmail.com>
2025-01-02 02:28:23 -0800

    Add: job card component on all jobs and latest 6 on home page

--- resources/views/jobs/index.blade.php
+++ resources/views/jobs/index.blade.php
@@ -1,9 +1,7 @@
 <x-layout>
-    <h1 class="text-2xl">All Jobs</h1>
-
     <div class="grid grid-cols-1 md:grid-cols-3 gap-4 mb-6">
         @forelse($jobs as $job)
-            <div>{{ $job->title }}</div>
+            <x-job-card :job="$job" />
         @empty
             <p>No jobs available</p>
         @endforelse
